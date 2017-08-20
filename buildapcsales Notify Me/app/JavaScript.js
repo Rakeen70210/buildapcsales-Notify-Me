32141d4b945094cf0543ae0375612fb4675cbebc
Rakeen Huq <rakeenhuq@hotmail.com>
2017-08-20 12:53:00 -0700

    commit

---
+++ buildapcsales Notify Me/app/JavaScript.js
@@ -0,0 +1,152 @@
+﻿document.addEventListener('DOMContentLoaded', function ()
+{
+    var reload = document.getElementById('Button2');
+
+    reload.addEventListener('click', function ()
+    {
+        chrome.runtime.reload();
+        console.log("Extension Reloaded");
+    });
+});
+
+//Wait for the document(popup.html) to fully load before executing any javascript
+document.addEventListener('DOMContentLoaded', function ()
+{
+    var start = document.getElementById('button1');
+    // onClick's logic below:
+    start.addEventListener('click', function ()
+    {
+        //Clear any previously stored data
+        chrome.storage.local.clear(function (callback)
+        {
+            //If there was an error clearing local storage.
+            var error = chrome.runtime.lastError;
+            if (error)
+            {
+                console.error(error);
+            }
+            else
+            {
+                console.log("local storage cleared");
+            }
+        });
+
+        var categories = document.getElementById("Select1");
+        var selectedCategory = categories.options[categories.selectedIndex].text;
+
+        url = checkCategory(selectedCategory);
+
+        url = url + ".json";
+
+        //Get the JSON data by using the specified url
+        getJSON(url);
+
+        //Store the url in local storage and set the alarm to activate every 1 minute
+        chrome.storage.local.set({ "url": url });
+        chrome.alarms.create("alarm", { delayInMinutes: 1, periodInMinutes: 1 });
+    });
+});
+
+
+function checkCategory(category)
+{
+    if(category === "All Categories")
+    {
+        return "https://www.reddit.com/r/buildapcsales/new/";
+    }
+    else if(category === "Case")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=Case&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "Controller")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=Controller&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "Cooler")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=cooler&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "CPU")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=CPU+OR+Processor&restrict_sr=on&sort=new&t=all";
+    }
+    else if(category === "FAN")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=Fan&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "GPU")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=GPU&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "HDD")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=GPU&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "Headphones")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=headphones&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "Keyboard")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=Keyboard&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category == "Monitor")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=monitor&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "Mouse")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=Mouse&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "PSU")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=PSU&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "RAM")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=RAM&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else  if(category === "SSD")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=SSD&sort=new&restrict_sr=on&feature=legacy_search";
+    }
+    else if(category === "MOBO")
+    {
+        return "https://www.reddit.com/r/buildapcsales/search?q=mobo+OR+motherboard&sort=new&restrict_sr=on&t=all";
+    }
+}
+//This function takes in a specified url with proper ".json" tag at the end of the url and proceeds to get JSON data back from the server
+function getJSON(url)
+{
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.send();
+    req.onreadystatechange = function ()
+    {
+        if (this.readyState === 4 && this.status === 200)
+        {
+            var jsonData = JSON.parse(this.responseText);
+            parseJSON(jsonData); //if data retreival is successful, call "parseJSON" function 
+        }
+    };
+}
+
+//This function parses the JSON data retrieved from the server and stores it all in an array
+function parseJSON(jsonData)
+{
+    var dataStorage = {}; //create an array to hold information from reddit
+
+    //now store the information obtained from the URL array, looping through all 25(default value) posts
+    for (var i = 0; i < 25; i++)
+    {
+
+        //get the title and url of each post
+        var title = JSON.stringify(jsonData.data.children[i].data.title);
+        var url = JSON.stringify(jsonData.data.children[i].data.url);
+        var thumbnail = JSON.stringify(jsonData.data.children[i].data.thumbnail);
+        //store the title and its respective url in each index of the array
+        dataStorage[title] = url;
+    }
+
+    chrome.storage.local.set({ data: dataStorage });
+}
---
+++ buildapcsales Notify Me/app/background.js
@@ -0,0 +1,117 @@
+﻿//Listen for alarm every 1 minute
+chrome.alarms.onAlarm.addListener(function (getJSON)
+{
+    //Get the specified URL stored in local storage
+    chrome.storage.local.get("url", function (obj)
+    {
+        var req = new XMLHttpRequest();
+        req.open("GET", obj.url, true);
+        req.send();
+        req.onreadystatechange = function ()
+        {
+            if (this.readyState === 4 && this.status === 200)
+            {
+                var jsonData = JSON.parse(this.responseText);
+                parseJSON(jsonData); //if data retreival is successful, call "parseJSON" function 
+            }
+        };
+    });
+});
+
+//This function parses the JSON data retrieved from the server and stores it all in an array
+function parseJSON(jsonData)
+{
+    var dataStorage = {}; //create an array to hold information from reddit
+    chrome.storage.local.get("data", function (obj)
+    {
+        //now store the information obtained from the URL array, looping through all 25(default value) posts
+        for (var i = 0; i < 25; i++)
+        {
+            //get the title and url of each post
+            var title = JSON.stringify(jsonData.data.children[i].data.title);
+            var url = JSON.stringify(jsonData.data.children[i].data.url);
+            var thumbnail = JSON.stringify(jsonData.data.children[i].data.thumbnail);
+
+            //if user has already been notified, i.e. there is collision in the table, keep track of item in new table
+            if (obj.data[title] === url)
+            {
+                dataStorage[title] = url;
+                console.log("item already in dataStorage");
+            }
+                //if user has not been notified and item was not in table, i.e. no collision, add item to new table and notify user
+            else if (obj.data[title] === undefined)
+            {
+                dataStorage[title] = url;
+                console.log("adding item to dataStorage");
+                console.log(title);
+
+                notification(removeQuotes(thumbnail), removeQuotes(url), title);
+            }
+
+            //if anything else is in the table and there was no collision, it is to be removed from the table.
+            //No actual removal occurs, just a new table is created and the old items are no longer in the new table.
+        }
+        console.log(dataStorage);
+        chrome.storage.local.set({ data: dataStorage });
+    });
+
+}
+
+//Remove quotes from urls obtained by JSON
+function removeQuotes(string)
+{
+    return string.toString().replace(/"/g, "");
+}
+
+var notificationID = null;
+//creates a notification on the desktop
+function notification(thumbnail, url, title)
+{
+    if (thumbnail == "self" || thumbnail == "default" || thumbnail == "nsfw" || thumbnail == "image")
+    {
+        chrome.notifications.create(url, {
+            type: "basic",
+            title: "",
+            message: title,
+            iconUrl: "icon.png",
+            buttons: [{ title: "/r/buildapcsales", iconUrl: "icon.png" }],
+            isClickable: true,
+            requireInteraction: true
+        });
+    }
+    else
+    {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', thumbnail, true);
+        xhr.responseType = 'blob';
+        xhr.onload = function (e)
+        {
+            chrome.notifications.create(url, {
+                type: "basic",
+                title: "",
+                message: title,
+                iconUrl: window.URL.createObjectURL(this.response),
+                buttons: [{ title: "/r/buildapcsales", iconUrl: "icon.png" }],
+                isClickable: true,
+                requireInteraction: true
+            });
+        };
+        xhr.send(null);
+    }
+}
+
+//User may click on the notification to take them to where the product sale was posted
+chrome.notifications.onClicked.addListener(function (notifID)
+{
+    chrome.tabs.create({ url: notifID });
+});
+
+chrome.notifications.onButtonClicked.addListener(function (replyBtnClick)
+{
+    chrome.storage.local.get("url", function (obj)
+    {
+        obj.url = (obj.url).replace(".json", "");
+        chrome.tabs.create({ url: obj.url });
+    });
+});
+
